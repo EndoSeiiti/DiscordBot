0382bfe2694dd583d1049bb9ffa390585c36bbfa
EndoSeiiti <endoseiiti@gmail.com>
2025-05-09 01:27:19 -0300

    feat:add basic Discord bot structure with app_commands support

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+from discord import app_commands
+import os
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN=os.getenv("DISCORD_TOKEN")
+
+class Morshu(discord.Client):
+    def __init__(self):
+        intents = discord.Intents.all()
+        super().__init__(
+            command_prefix= "$",
+            intents=intents
+
+        )
+        self.tree = app_commands.CommandTree(self)
+
+
+
+    async def setup_hook(self):
+        await self.tree.sync()
+    
+    async def on_ready(self):
+        print(f"Lamp oil, Ropes, Bombs? You want it?")
+
+bot = Morshu()
+
+bot.run(TOKEN)
